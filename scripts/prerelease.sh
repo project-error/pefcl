c6b137eec23a9122baf7a2e8f1fa83d91eec0bae
Anton Stjernquist <antonstjernquist@gmail.com>
2022-03-14 23:57:10 +0100

    fix(ci/release): made it work

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -7,7 +7,7 @@ echo "$PREFIX Creating release"
 ## Create temporary folder and move files into it. Keeping same structure according to fxmanifest.lua
 mkdir -p ./temp/$RESOURCE/src
 mkdir -p ./temp/$RESOURCE/web
-cp LICENSE README.md config.json import.sql fxmanifest.lua ./temp
+cp LICENSE README.md config.json import.sql fxmanifest.lua ./temp/$RESOURCE
 cp -r ./src/dist ./temp/$RESOURCE/src/dist # Copy resource files
 cp -r ./web/dist ./temp/$RESOURCE/web/dist # Copy web files
 
