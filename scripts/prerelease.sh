4de2d632579b9b6b82310f7c56635cd6a853c299
Anton Stjernquist <antonstjernquist@gmail.com>
2022-03-14 23:07:14 +0100

    fix(ci/release): added manual trigger(?), perm fix

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -7,10 +7,10 @@ echo "$PREFIX Creating release"
 ## Create temporary folder and move files into it. Keeping same structure according to fxmanifest.lua
 mkdir -p ./temp/$RESOURCE/src
 mkdir -p ./temp/$RESOURCE/web
-cp ./{LICENSE,README.md,config.json,import.sql,fxmanifest.lua} ./temp/$RESOURCE
+cp LICENSE README.md config.json import.sql fxmanifest.lua ./temp
 cp -r ./src/dist ./temp/$RESOURCE/src/dist # Copy resource files
 cp -r ./web/dist ./temp/$RESOURCE/web/dist # Copy web files
 
-echo "$PREFIX Zipping it up"
+echo "$PREFIX Zipping it up: $RESOURCE-pre-$GITHUB_SHA_SHORT.zip"
 
-cd ./temp && zip -r $RESOURCE-pre-${{ steps.get_vars.outputs.SHORT_SHA }}.zip ./$RESOURCE
+cd temp && zip -r $RESOURCE-pre-$GITHUB_SHA_SHORT.zip ./$RESOURCE
