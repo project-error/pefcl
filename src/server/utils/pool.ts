c13fe543192907e02d07c87177ad585b4486294f
Anton Stjernquist <antonstjernquist@gmail.com>
2022-04-28 22:18:40 +0200

    fix: stole npwd DB parser

--- src/server/utils/pool.ts
+++ src/server/utils/pool.ts
@@ -1,17 +1,25 @@
 import { Sequelize } from 'sequelize';
-import { CONNECTION_STRING } from './dbUtils';
+import { CONNECTION_STRING, parseUri } from './dbUtils';
 
 const mysqlConnectionString = GetConvar(CONNECTION_STRING, 'none');
+
 if (mysqlConnectionString === 'none') {
   throw new Error(
     `No connection string provided. make sure "${CONNECTION_STRING}" is set in server.cfg`,
   );
 }
 
-export const sequelize = new Sequelize(mysqlConnectionString, {
+const config = parseUri(mysqlConnectionString);
+
+export const sequelize = new Sequelize({
   dialect: 'mysql',
   dialectModule: require('mysql2'),
   logging: false,
+  host: config.host,
+  port: typeof config.port === 'string' ? parseInt(config.port, 10) : config.port,
+  username: config.user,
+  password: config.password,
+  database: config.database,
   pool: {
     max: 5,
     min: 0,
