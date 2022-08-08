4032670dc8a0e5b06f0eaab03b7a5e96a83cc6b4
Anton Stjernquist <antonstjernquist@gmail.com>
2022-08-08 13:45:04 +0200

    feat: help message for accounts view mobile

--- web/src/views/Mobile/views/Accounts/MobileAccountsView.tsx
+++ web/src/views/Mobile/views/Accounts/MobileAccountsView.tsx
@@ -1,12 +1,15 @@
 import { AccountCard } from '@components/Card';
 import TotalBalance from '@components/TotalBalance';
+import { Heading5 } from '@components/ui/Typography/Headings';
 import { accountsAtom } from '@data/accounts';
 import { Stack } from '@mui/material';
 import { Box } from '@mui/system';
 import { useAtom } from 'jotai';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const MobileAccountsView = () => {
+  const { t } = useTranslation();
   const [accounts] = useAtom(accountsAtom);
 
   return (
@@ -19,6 +22,12 @@ const MobileAccountsView = () => {
             return <AccountCard account={account} key={account.id} />;
           })}
         </Stack>
+
+        {accounts.length <= 1 && (
+          <Stack spacing={1}>
+            <Heading5>{t('You can create more accounts by visiting the nearest bank ..')}</Heading5>
+          </Stack>
+        )}
       </Stack>
     </Box>
   );
