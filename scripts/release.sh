9715af9e0dcdaade4346629b4e01fe5e1cb6cd9e
Anton Stjernquist <antonstjernquist@gmail.com>
2022-09-10 21:56:45 +0200

    fix: remove prerelease hook

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,11 +5,11 @@ RESOURCE="pefcl"
 
 echo "$PREFIX Creating release"
 ## Create temporary folder and move files into it. Keeping same structure according to fxmanifest.lua
-mkdir -p ./temp/$RESOURCE/src
+mkdir -p ./temp/$RESOURCE/src/client
 mkdir -p ./temp/$RESOURCE/web
 cp LICENSE README.md config.json import.sql fxmanifest.lua ./temp/$RESOURCE
 cp -r ./src/dist ./temp/$RESOURCE/src/dist # Copy resource files
-cp -r ./src/client/lua ./temp/$RESOURCE/src/client/lua # Copy Lua files
+cp -r ./src/client/lua/ ./temp/$RESOURCE/src/client/lua # Copy Lua files
 cp -r ./web/dist ./temp/$RESOURCE/web/dist # Copy web files
 
 echo "$PREFIX Zipping it up: $RESOURCE.zip"
