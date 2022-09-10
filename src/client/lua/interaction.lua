84a08a3d1d92691256d75c9740050f7b790ffafd
Oldarorn <36448848+Oldarorn@users.noreply.github.com>
2022-09-11 00:07:02 +0200

    Adding ATMs support when target.enabled is false (#72)
    * Adding ATMs support when target.enabled is false
    
    I tried to do something clean, it doesn't display any markers but it'll show Help notification when you're close to an atm
    
    * Fix fails
    
    * Cleaning things
    
    Raw text is used, I had to edit the helping part because ClearHelp didn't wanted to do a proper thing, so, everything's working now.

--- src/client/lua/interaction.lua
+++ src/client/lua/interaction.lua
@@ -1,15 +1,15 @@
 local config = json.decode(LoadResourceFile(GetCurrentResourceName(), "config.json"))
 local bank_coords = config.bankBlips.coords
+local atm_props = config.atms.props
 
-
-function display_help_text()
+function display_help_text(text)
   BeginTextCommandDisplayHelp("STRING")
-  AddTextComponentString("Open bank: ~INPUT_PICKUP~")
-  EndTextCommandDisplayHelp(0, true, false, -1)
+  AddTextComponentString(text)
+  EndTextCommandDisplayHelp(0, false, false, -1)
 end
 
 CreateThread(function ()
-    
+
   if not config.target.enabled then
     while true do
       local player_id = PlayerPedId()
@@ -19,20 +19,34 @@ CreateThread(function ()
       for i = 1, #bank_coords do
         local pos = bank_coords[i]
 
-        local dist = #(player_coords - vector3(pos.x, pos.y, pos.z))
-        if dist <= 10.0 then
+        local distBank = #(player_coords - vector3(pos.x, pos.y, pos.z))
+        if distBank <= 10.0 then
           DrawMarker(2, pos.x, pos.y, pos.z, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 0.5, 0.5, 0.3, 255, 255, 255, 50, false, true, 2, nil, nil, false)
 
-          if dist <= 3.5 then
-            display_help_text()
+          if distBank <= 3.5 then
+            display_help_text("Open bank: ~INPUT_PICKUP~")
+            
 
             if IsControlJustReleased(0, 38) then
               exports["pefcl"]:openBank()
             end
-          else
-            ClearHelp(true)
           end
 
+          sleep = 0
+        end
+      end
+
+      for i = 1, #atm_props do
+        local prop = GetClosestObjectOfType(player_coords, 5.0, joaat(atm_props[i]), false, false, false)
+        local pos = GetEntityCoords(prop)
+        local distAtm = #(player_coords - pos)
+        
+        if distAtm <= 2.0 then
+          display_help_text("Open atm: ~INPUT_PICKUP~")
+          
+          if IsControlJustReleased(0, 38) then
+            exports["pefcl"]:openAtm()
+          end
 
           sleep = 0
         end
