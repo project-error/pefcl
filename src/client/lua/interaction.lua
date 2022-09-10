d8230829e8c0904975676c824fb0c464151e9f4d
Oldarorn <36448848+Oldarorn@users.noreply.github.com>
2022-09-10 23:25:28 +0200

    Cleaning things
    Raw text is used, I had to edit the helping part because ClearHelp didn't wanted to do a proper thing, so, everything's working now.

--- src/client/lua/interaction.lua
+++ src/client/lua/interaction.lua
@@ -1,12 +1,11 @@
 local config = json.decode(LoadResourceFile(GetCurrentResourceName(), "config.json"))
 local bank_coords = config.bankBlips.coords
 local atm_props = config.atms.props
-local text
 
 function display_help_text(text)
   BeginTextCommandDisplayHelp("STRING")
   AddTextComponentString(text)
-  EndTextCommandDisplayHelp(0, true, false, -1)
+  EndTextCommandDisplayHelp(0, false, false, -1)
 end
 
 CreateThread(function ()
@@ -20,21 +19,18 @@ CreateThread(function ()
       for i = 1, #bank_coords do
         local pos = bank_coords[i]
 
-        local dist = #(player_coords - vector3(pos.x, pos.y, pos.z))
-        if dist <= 10.0 then
+        local distBank = #(player_coords - vector3(pos.x, pos.y, pos.z))
+        if distBank <= 10.0 then
           DrawMarker(2, pos.x, pos.y, pos.z, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 0.5, 0.5, 0.3, 255, 255, 255, 50, false, true, 2, nil, nil, false)
 
-          if dist <= 3.5 then
-              text = "Open bank: ~INPUT_PICKUP~"
-              display_help_text(text)
+          if distBank <= 3.5 then
+            display_help_text("Open bank: ~INPUT_PICKUP~")
+            
 
             if IsControlJustReleased(0, 38) then
               exports["pefcl"]:openBank()
             end
-          else
-            ClearHelp(true)
           end
-
 
           sleep = 0
         end
@@ -43,19 +39,16 @@ CreateThread(function ()
       for i = 1, #atm_props do
         local prop = GetClosestObjectOfType(player_coords, 5.0, joaat(atm_props[i]), false, false, false)
         local pos = GetEntityCoords(prop)
-        local dist = #(player_coords - pos)
+        local distAtm = #(player_coords - pos)
         
-        if dist <= 2.0 then
-          text = "Open atm: ~INPUT_PICKUP~"
-          display_help_text(text)
-
+        if distAtm <= 2.0 then
+          display_help_text("Open atm: ~INPUT_PICKUP~")
+          
           if IsControlJustReleased(0, 38) then
             exports["pefcl"]:openAtm()
           end
 
           sleep = 0
-        else
-          ClearHelp(true)
         end
       end
 
