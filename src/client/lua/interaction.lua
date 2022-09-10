1c85806ccac969d1a3ab18acc8508f0734826b22
Oldarorn <36448848+Oldarorn@users.noreply.github.com>
2022-09-10 22:41:03 +0200

    Adding ATMs support when target.enabled is false
    I tried to do something clean, it doesn't display any markers but it'll show Help notification when you're close to an atm

--- src/client/lua/interaction.lua
+++ src/client/lua/interaction.lua
@@ -1,15 +1,16 @@
 local config = json.decode(LoadResourceFile(GetCurrentResourceName(), "config.json"))
 local bank_coords = config.bankBlips.coords
+local atm_props = config.atms.props
+local text
 
-
-function display_help_text()
+function display_help_text(text)
   BeginTextCommandDisplayHelp("STRING")
-  AddTextComponentString("Open bank: ~INPUT_PICKUP~")
+  AddTextComponentString(text)
   EndTextCommandDisplayHelp(0, true, false, -1)
 end
 
 CreateThread(function ()
-    
+
   if not config.target.enabled then
     while true do
       local player_id = PlayerPedId()
@@ -24,7 +25,8 @@ CreateThread(function ()
           DrawMarker(2, pos.x, pos.y, pos.z, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 0.5, 0.5, 0.3, 255, 255, 255, 50, false, true, 2, nil, nil, false)
 
           if dist <= 3.5 then
-            display_help_text()
+            local text = "Open bank: ~INPUT_PICKUP~"
+            display_help_text(text)
 
             if IsControlJustReleased(0, 38) then
               exports["pefcl"]:openBank()
@@ -35,6 +37,25 @@ CreateThread(function ()
 
 
           sleep = 0
+        end
+      end
+
+      for i = 1, #atm_props do
+        local prop = GetClosestObjectOfType(player_coords, 5.0, joaat(atm_props[i]), false, false, false)
+        local pos = GetEntityCoords(prop)
+        local dist = #(player_coords - pos)
+        
+        if dist <= 2.0 then
+          local text = "Open atm: ~INPUT_PICKUP~"
+          display_help_text(text)
+
+          if IsControlJustReleased(0, 38) then
+            exports["pefcl"]:openAtm()
+          end
+
+          sleep = 0
+        else
+          ClearHelp(true)
         end
       end
 
